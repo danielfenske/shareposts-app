7e6bdd606c98aca0aec60da279a90693b62af43e
Dan Fenske <dlfenske.fenske@gmail.com>
2022-05-22 17:39:51 -0500

    Initial commit

---
+++ app/libraries/Controller.php
@@ -0,0 +1,31 @@
+<?php
+    /*
+     * Base Controller, which loads Models and Views
+     */
+
+    class Controller
+    {
+//        Load model
+        public function model($model)
+        {
+//            Require model file
+            require_once '../app/models/' . $model . '.php';
+
+//            Instantiate model
+            return new $model();
+        }
+
+//        Load view
+        public function view($view, $data = [])
+        {
+//            Check for view file
+            if (file_exists('../app/views/' . $view . '.php'))
+            {
+                require_once '../app/views/' . $view . '.php';
+            }
+            else
+            {
+                die('View does not exist');
+            }
+        }
+    }
