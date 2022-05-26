bcc63b64dfd0ca5d573fb237cb2a2dd1bc4051ec
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-05-26 10:50:55 -0500

    Set up and test Delete Post feature in app

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -126,8 +126,8 @@
                 }
 
             } else
-//                Get existing post from model
-                $post = $this->postModel->getPostById($id);
+//            Get existing post from model
+            $post = $this->postModel->getPostById($id);
 //            Check for owner
             if($post->user_id !== $_SESSION['user_id'])
             {
@@ -155,5 +155,32 @@
             ];
 
             $this->view('posts/show', $data);
+        }
+
+        public function delete($id)
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST')
+            {
+
+//            Get existing post from model
+                 $post = $this->postModel->getPostById($id);
+//            Check for owner
+              if($post->user_id !== $_SESSION['user_id'])
+              {
+                  redirect('posts');
+              }
+
+              if($this->postModel->deletePost($id))
+              {
+                  flash('post_message', 'Post removed!');
+                  redirect('posts');
+              } else
+              {
+                  die('Something went wrong');
+              }
+            } else
+            {
+                redirect('posts');
+            }
         }
     }
